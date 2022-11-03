221c9967e94bd8e55c9b49f9e4cad1994c7ba1fd
Student <student@iMac-Astrum-338.local>
2022-11-03 14:07:34 +0500

    I added C and python floder

---
+++ C/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //standart input output header
+#include <stdlib.h> //standart library header
+
+int main() // asosiy funksya
+{
+    printf("Hello worild!\n"); // print funksyasi
+    printf("Salom C dasturlash tili!");
+    
+    return 0; // dastur muvaffaqiyatli tugaganini bildradi
+}
