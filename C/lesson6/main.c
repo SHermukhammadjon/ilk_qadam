5c8ecde75907b7567a8ae556af4e98e6ad803588
Student <student@iMac-Astrum-338.local>
2022-11-04 13:45:13 +0500

    I finished lesson 6 main.c file

---
+++ C/lesson6/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    // char serya[10];
+    // printf("Seriyani kiriting: ");
+    // scanf("%s", &serya);
+    // printf("\nSeriyangiz: %s\n", serya);
+    
+    //GETS VA PUTS
+
+    // gets(serya); //char belgilar
+    // puts(serya);
+    
+    //GETC VA PUTC
+
+    char belgi;
+    // getc(belgi);
+    // putc(belgi);
+    scanf(" %c", &belgi);
+    // putc(belgi);
+    printf("%c\n", belgi);
+
+
+
+    return 0;
+}
